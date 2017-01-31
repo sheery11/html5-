3fcb6da12a708095cbf8548fda74aec7d9e78921
sheery11 <806161245@qq.com>
2017-01-31 14:48:35 +0800

    You can see the effect on the mobile side

---
+++ enroll.js
@@ -0,0 +1,6 @@
+var enroll = document.getElementById("enroll");
+enroll.onclick = function(){
+	enroll.style.background = "#27cb8b";
+	enroll.style.borderColor = "#27cb8b";
+	enroll.innerHTML = "报名成功";
+}
